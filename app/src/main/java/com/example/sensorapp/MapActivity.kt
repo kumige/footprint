99f6c32dc7e987c8f3f75c2d2651305e898afa8e
Janne Suotsalo <janne.suotsalo@gmail.com>
2019-09-26 15:17:47 +0300

    Added map activity

---
+++ app/src/main/java/com/example/sensorapp/MapActivity.kt
@@ -0,0 +1,24 @@
+package com.example.sensorapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Map"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
