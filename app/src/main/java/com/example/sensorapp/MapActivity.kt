749e6d72d6e25af4c6c8a7420f15d181ab157584
Janne Suotsalo <janne.suotsalo@gmail.com>
2019-09-26 15:58:09 +0300

    Added map into MapActivity

--- app/src/main/java/com/example/sensorapp/MapActivity.kt
+++ app/src/main/java/com/example/sensorapp/MapActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sensorapp
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_map.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 
 class MapActivity : AppCompatActivity() {
 
@@ -15,7 +19,17 @@ class MapActivity : AppCompatActivity() {
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
+
+        // Map
+        val ctx = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
+        map.controller.setZoom(18.0)
     }
+
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
